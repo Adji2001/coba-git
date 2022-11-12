2ca966d787d93d14c2ba9b7bafd8354cb3e3b536
Adji2001 <abdulm4j1d182@gmail.com>
2022-11-11 18:32:18 -0800

    membuat file index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>title php</title>
+</head>
+<body>
+	<h1>test aja yah</h1>
+</body>
+</html>
